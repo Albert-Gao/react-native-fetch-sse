87fa42980763d18f5abe5a8383500334f35c74bd
Albert Gao <albertgaohy@gmail.com>
2023-12-21 01:27:01 +0000

    refactor: 🔍️ now we are using streaming to handle all response cases (error/non-error)

---
+++ src/streams.ts
@@ -0,0 +1,68 @@
+import { StreamError } from './StreamError';
+import { EventSourceParserStream } from './EventSourceParserStream';
+
+export function createEmptyReadableStream(): ReadableStream {
+  return new ReadableStream({
+    start(controller) {
+      controller.close();
+    },
+  });
+}
+
+/** this function triggers an error with errorMessage */
+function errorStream(errMsg: string) {
+  return new ReadableStream({
+    start(controller) {
+      controller.error(new Error(errMsg));
+    },
+  });
+}
+
+/** this function consumes the error stream (error message that sends as a SSE event)  */
+function errorEventStream(response: Response) {
+  const reader = response.body?.getReader();
+
+  return new ReadableStream({
+    async start(controller) {
+      if (!reader) {
+        controller.error(new Error(`Response error: no reader found`));
+        return;
+      }
+
+      const { done, value } = await reader.read();
+
+      if (!done) {
+        const errorText = new TextDecoder().decode(value);
+        controller.error(
+          new StreamError(errorText, response.status, response.statusText)
+        );
+      }
+    },
+  });
+}
+
+/** this function handles the 200 and non-200 cases, and make it all returns a stream */
+export function consumeStream(
+  response: Response,
+  shouldParseJsonWhenOnMsg: boolean
+) {
+  if (!response) {
+    return errorStream('Response error: No response');
+  }
+
+  if (!response.ok) {
+    if (!response.body) {
+      return errorStream('Response error: No response body');
+    }
+
+    return errorEventStream(response);
+  }
+
+  const body = response.body || createEmptyReadableStream();
+
+  const piped = body.pipeThrough(
+    new EventSourceParserStream({ shouldParseJsonWhenOnMsg })
+  );
+
+  return piped;
+}
